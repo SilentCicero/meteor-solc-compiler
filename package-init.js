e0b09c192de08e774d50676a2674158fa6da2f71
Nick Dodson <silentcicero@outlook.com>
2015-10-17 15:58:43 -0400

    Update package-init.js

--- package-init.js
+++ package-init.js
@@ -1,3 +1,8 @@
+// Browser environment
+if(typeof window !== 'undefined') {
+    solc = (typeof window.solc !== 'undefined') ? window.solc : require('solc');
+}
+
 // Node environment
 if(typeof global !== 'undefined') {
     solc = (typeof global.solc !== 'undefined') ? global.solc : Npm.require('solc');
